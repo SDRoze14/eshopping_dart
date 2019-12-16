8e3b25bc1ede228dcefcfd197ef0b770d19b94dc
SDRoze14 <nratchakomm@gmail.com>
2019-12-16 13:22:40 +0700

    product details

--- lib/pages/product_details.dart
+++ lib/pages/product_details.dart
@@ -6,12 +6,11 @@ class ProductDetails extends StatefulWidget {
   final product_detail_new_price;
   final product_detail_picture;
 
-  ProductDetails({
-    this.product_detail_name,
-    this.product_detail_new_price,
-    this.product_detail_old_price,
-    this.product_detail_picture
-  });
+  ProductDetails(
+      {this.product_detail_name,
+      this.product_detail_new_price,
+      this.product_detail_old_price,
+      this.product_detail_picture});
 
   @override
   _ProductDetailsState createState() => _ProductDetailsState();
@@ -22,24 +21,109 @@ class _ProductDetailsState extends State<ProductDetails> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-          backgroundColor: Colors.green,
-          title: Text('eSopping'),
-          actions: <Widget>[
-            new IconButton(
-              icon: Icon(Icons.shopping_cart),
-              onPressed: (){},
-            )
-          ],
-        ),
+        backgroundColor: Colors.green,
+        title: Text('eSopping'),
+        actions: <Widget>[
+          new IconButton(
+            icon: Icon(Icons.shopping_cart),
+            onPressed: () {},
+          )
+        ],
+      ),
+      body: new ListView(
+        children: <Widget>[
+          new Container(
+            height: 300.0,
+            child: GridTile(
+              child: Container(
+                color: Colors.white,
+                child: Image.asset(widget.product_detail_picture),
+              ),
+              footer: new Container(
+                color: Colors.white70,
+                child: ListTile(
+                  leading: new Text(
+                    widget.product_detail_name,
+                    style:
+                        TextStyle(fontWeight: FontWeight.bold, fontSize: 16.0),
+                  ),
+                  title: new Row(
+                    children: <Widget>[
+                      Expanded(
+                        child: new Text(
+                            '\u0E3F${widget.product_detail_old_price}',
+                            style: TextStyle(
+                                color: Colors.grey,
+                                decoration: TextDecoration.lineThrough)),
+                      ),
+                      Expanded(
+                        child: new Text(
+                          '\u0E3F${widget.product_detail_new_price}',
+                          style: TextStyle(
+                              color: Colors.red, fontWeight: FontWeight.bold),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ),
 
-        body: new ListView(
-          children: <Widget>[
-            new Container(
-              height: 300.0,
-              color: Colors.black,
-            )
-          ],
-        ),
+          // ============== First Button ==============
+
+          Row(
+            children: <Widget>[
+              // Size Button
+              Expanded(
+                child: MaterialButton(
+                  onPressed: () {},
+                  color: Colors.white,
+                  textColor: Colors.grey,
+                  child: Row(
+                    children: <Widget>[
+                      Expanded(child: new Text('Size')),
+                      Expanded(child: new Icon(Icons.arrow_drop_down)),
+                    ],
+                  ),
+                ),
+              ),
+
+              // Color Button 
+              Expanded(
+                child: MaterialButton(
+                  onPressed: () {},
+                  color: Colors.white,
+                  textColor: Colors.grey,
+                  child: Row(
+                    children: <Widget>[
+                      Expanded(child: new Text('Color')),
+                      Expanded(child: new Icon(Icons.arrow_drop_down)),
+                    ],
+                  ),
+                ),
+              ),
+
+              // Quality Button 
+              Expanded(
+                child: MaterialButton(
+                  onPressed: () {},
+                  color: Colors.white,
+                  textColor: Colors.grey,
+                  child: Row(
+                    children: <Widget>[
+                      Expanded(child: new Text('Qty')),
+                      Expanded(child: new Icon(Icons.arrow_drop_down)),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+
+          // =============== Second Button ================
+        ],
+      ),
     );
   }
 }
